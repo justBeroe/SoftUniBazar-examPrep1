2f241858fe9b40cb21d605d0e0c5db39ee5c4cbc
Just Beroe <dobromirtt@gmail.com>
2025-04-06 00:31:48 +0300

    Update to .NET 8

--- SoftUniBazar/obj/Debug/net6.0/SoftUniBazar.AssemblyInfo.cs
+++ SoftUniBazar/obj/Debug/net6.0/SoftUniBazar.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SoftUniBazar")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c425415e9766bd2d0419fbab1c50a6f2dcda3f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SoftUniBazar")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SoftUniBazar")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
